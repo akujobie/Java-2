dc4431a793bbebaaac9c41936703f38030e806b0
akujobie <ebere24akujobi@gmail.com>
2024-03-25 19:35:34 +0100

    Initial commit

--- Lottery.js
+++ Lottery.js
@@ -18,7 +18,7 @@ function getCredentials() {
 // Function to generate a random number between two integers
 function generateRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+  };
 
 // Function to handle the lottery game
  letsplay.onclick=() =>{
